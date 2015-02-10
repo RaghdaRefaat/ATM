fe10557555eb264e4831a8a052b789eb5bc54c3e
RaghdaRefaat <raghda.refaat2011@gmail.com>
2015-02-11 00:02:16 +0200

    New Client Part

---
+++ Client.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ATM
+{
+    class Client
+    {
+        private string Name;
+        private int ID;
+        private float Balance;
+
+        public int counter;
+
+        public Client()
+        {
+            Name = " " ;
+            ID = 0 ;
+            Balance = 0 ;
+        }
+
+        public void SetName(string name)
+        { Name = name ; }
+
+        public void SetID(int id)
+        { ID = id ; }
+
+        public void SetBalance(float bal)
+        { Balance = bal ; }
+
+        public string GetName()
+        { return Name; }
+
+        public int GetID()
+        { return ID; }
+
+        public float GetBalance()
+        { return Balance; }
+
+        public void AddClient()
+        {
+            FileStream fs = new FileStream("D://ClientData.txt", FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs);
+            string name = " ";
+            float bal = 0;
+            Console.WriteLine("Please Enter Your Name !");
+            name = Console.ReadLine();
+            Console.WriteLine("Please Enter Your Balance !");
+            bal = int.Parse(Console.ReadLine());
+            if (bal < 1)
+            {
+                Console.WriteLine("invalid balance , please enter another one");
+                bal = int.Parse(Console.ReadLine());
+            }
+            counter++;
+            Console.Write("Your ID is : ");
+            Console.WriteLine(counter);
+            SetName(name);
+            SetID(counter);
+            SetBalance(bal);
+            sw.WriteLine(Name);
+            sw.WriteLine(counter);
+            sw.WriteLine(Balance);
+            sw.Close();
+        }
+
+        public bool SearchID(int id)
+        {
+            FileStream f = new FileStream("D://ClientData.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(f);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            int choice = 0;
+            int id = 0;
+            Console.WriteLine("** Welcome At Auto Teller Machine **");
+            Console.WriteLine("Tell Us Who Are You ?!");
+            Console.WriteLine("1- New Client ! ");
+            Console.WriteLine("2- Existing Client !");
+            choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1 :
+                    client.AddClient();
+                    break;
+                case 2:
+                    Console.WriteLine("Please enter Your ID !");
+                    id = int.Parse(Console.ReadLine());
+                    if (id < client.counter)
+                    { }
+                    else
+                        Console.WriteLine("Sorry invalid ID ! , You Have 3 trials only");
+
+                    break;
+            }
+        }
+    }
+}
